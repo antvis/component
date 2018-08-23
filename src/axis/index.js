a8c9e4cd2f59303d56995ff32b24aff63ce5ca56
leungwensen <leungwensen@gmail.com>
2018-08-23 16:28:32 +0800

    chore(axis): moving code from G2 to component

--- src/axis/index.js
+++ src/axis/index.js
@@ -1,6 +1,11 @@
 
 const Axis = require('./base');
 
+Axis.Base = Axis;
+Axis.Grid = require('./grid');
+Axis.Helix = require('./helix');
+Axis.Polar = require('./polar');
+Axis.Polyline = require('./polyline');
 Axis.Rect = require('./rect');
 
 module.exports = Axis;
