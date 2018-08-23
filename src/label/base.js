0db4da346123ca068b6a232fea6021c1cd2baa89
Elaine1234 <yilin.qyl@antfin.com>
2018-08-23 17:26:05 +0800

    feat: update label group instead of create all label insts

--- src/label/base.js
+++ src/label/base.js
@@ -49,6 +49,10 @@ class Label extends Component {
       labelLine: false
     });
   }
+
+  /*
+   * 清空label容器
+   */
   clear() {
     const group = this.get('group');
     const container = this.get('container');
@@ -60,12 +64,20 @@ class Label extends Component {
     }
     super.clear();
   }
+
+  /**
+   * 销毁group
+   */
   destroy() {
     const group = this.get('group');
     if (!group.destroy) {
       group.destroy();
     }
   }
+
+  /**
+   * label绘制全过程
+   */
   render() {
     this.clear();
     this._init();
@@ -73,18 +85,76 @@ class Label extends Component {
     this.draw();
     this.afterDraw();
   }
+
+  /**
+   * 更新label
+   * 1. 将items与group中的children对比，更新/新增/删除labels
+   * 2. labels布局优化
+   * 3. 画label连接线
+   * 4. 绘制到画布
+   */
   draw() {
     const self = this;
     const items = self.get('items');
-    const labels = Util.each(items, function(item, index) {
-      self._addLabel(item, index);
+    const children = self.getLabels();
+    const count = children.length;
+    Util.each(items, function(item, index) {
+      if (index < count) {
+        const label = children[index];
+        self.changeLabel(label, item, index);
+      } else {
+        const labelShape = self._addLabel(item, index);
+        if (labelShape) {
+          labelShape._id = item._id;
+          labelShape.set('coord', item.coord);
+        }
+      }
     });
-    this._adjustLabels(labels);
-    if (labels.labelLine) {
+    for (let i = count - 1; i >= items.length; i--) {
+      children[i].remove();
+    }
+    this._adjustLabels();
+    if (self.get('labelLine')) {
       self.drawLines();
     }
     this.get('canvas').draw();
   }
+
+  /*
+   * 更新label
+   * oldLabel shape或label dom
+   * newLabel label data
+   * index items中的下标
+   */
+  changeLabel(oldLabel, newLabel, index) {
+    if (!oldLabel) {
+      return;
+    }
+    const htmlTemplate = this.get('htmlTemplate');
+    const cfg = this._getLabelCfg(newLabel, index);
+    if (htmlTemplate) {
+      const node = this._createDom(cfg);
+      oldLabel.innerHTML = node.innerHTML;
+      this._setCustomPosition(cfg, oldLabel);
+    } else {
+      oldLabel._id = newLabel._id;
+      oldLabel.attr('text', cfg.text);
+      if (oldLabel.attr('x') !== cfg.x || oldLabel.attr('y') !== cfg.y) {
+        const rotate = oldLabel.get('attrs').rotate;
+        if (rotate) {
+          oldLabel.rotateAtStart(-rotate);
+          oldLabel.attr(cfg);
+          oldLabel.rotateAtStart(rotate);
+        } else {
+          oldLabel.attr(cfg);
+        }
+      }
+    }
+  }
+
+  /**
+   * 显示label
+   */
   show() {
     const group = this.get('group');
     const container = this.get('container');
@@ -95,6 +165,9 @@ class Label extends Component {
       container.style.opacity = 1;
     }
   }
+  /**
+   * 隐藏label
+   */
   hide() {
     const group = this.get('group');
     const container = this.get('container');
@@ -105,6 +178,10 @@ class Label extends Component {
       container.style.opacity = 0;
     }
   }
+
+  /**
+   * 画label连接线
+   */
   drawLines() {
     const self = this;
     Util.each(self.get('items'), label => {
@@ -142,6 +219,11 @@ class Label extends Component {
   }
   // TODO 区分label的type or 定成一个配置项用util方法？
   _adjustLabels() {}
+
+  /**
+   * 获取当前所有label实例
+   * @return {Array} 当前label实例
+   */
   getLabels() {
     const container = this.get('container');
     if (container) {
@@ -149,10 +231,16 @@ class Label extends Component {
     }
     return this.get('group').get('children');
   }
+
+  // 先计算label的所有配置项，然后生成label实例
   _addLabel(item, index) {
     const cfg = this._getLabelCfg(item, index);
     return this._createText(cfg);
   }
+
+  /**
+   * label初始化，主要针对html容器
+   */
   _init() {
     if (this.get('htmlTemplate')) {
       let container = this.get('container');
@@ -172,6 +260,7 @@ class Label extends Component {
       }
     }
   }
+  // 计算label的configs
   _getLabelCfg(item, index) {
     const self = this;
     let textStyle = self.get('textStyle') || {};
@@ -208,6 +297,7 @@ class Label extends Component {
     });
     return cfg;
   }
+  // 分html dom和G shape两种情况生成label实例
   _createText(cfg) {
     const htmlTemplate = this.get('htmlTemplate');
     const container = this.get('container');
@@ -237,11 +327,10 @@ class Label extends Component {
     if (Util.isString(htmlTemplate)) {
       cfg.text = Util.substitute(htmlTemplate, { text: cfg.text });
     }
-
     const str = Util.substitute(itemTpl, { text: cfg.text });
-
     return DomUtil.createDom(str);
   }
+  // 根据文本对齐方式确定dom位置
   _setCustomPosition(cfg, htmlDom) {
     const textAlign = cfg.textAlign || 'left';
     let top = cfg.y;
