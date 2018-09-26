7d72ee5585d515f53e52905c244cd4f7900d93c7
Elaine1234 <yilin.qyl@antfin.com>
2018-09-26 21:38:02 +0800

    feat(Greedy): add greedy placement for scatter

---
+++ src/label/utils/greedy.js
@@ -0,0 +1,101 @@
+
+const bitmap = [];
+
+class Greedy {
+  hasGap(bbox) {
+    let area = 0;
+    for (let i = Math.floor(bbox.minX); i < Math.ceil(bbox.maxX); i++) {
+      if (!bitmap[i]) {
+        bitmap[i] = [];
+        continue;
+      }
+      for (let j = Math.floor(bbox.minY); j < Math.ceil(bbox.maxY); j++) {
+        if (bitmap[i][j]) {
+          area += 1;
+          break;
+        }
+      }
+    }
+
+    if (area === 0) {
+      return true;
+    }
+    return false;
+  }
+  fillGap(bbox) {
+    for (let i = Math.floor(bbox.minX); i < Math.ceil(bbox.maxX); i++) {
+      for (let j = Math.floor(bbox.minY); j < Math.ceil(bbox.maxY); j++) {
+        bitmap[i][j] = true;
+      }
+    }
+  }
+  adjustLabelPosition(label, x, y, index) {
+    const bbox = label.getBBox();
+    const width = bbox.width;
+    const height = bbox.height;
+    const attrs = {
+      x,
+      y,
+      textAlign: 'center'
+    };
+    switch (index) {
+      case 0:
+        attrs.y -= height / 2;
+        attrs.textAlign = 'left';
+        break;
+      case 1:
+        attrs.y -= height / 2;
+        attrs.textAlign = 'right';
+        break;
+      case 2:
+        attrs.y += height / 2;
+        attrs.textAlign = 'right';
+        break;
+      case 3:
+        attrs.y += height / 2;
+        attrs.textAlign = 'left';
+        break;
+      case 4:
+        attrs.x += width / 2;
+        attrs.textAlign = 'left';
+        break;
+      case 5:
+        attrs.y -= height / 2;
+        break;
+      case 6:
+        attrs.x -= width / 2;
+        attrs.textAlign = 'right';
+        break;
+      case 7:
+        attrs.y += height / 2;
+        break;
+      default:break;
+    }
+    label.attr(attrs);
+    return label.getBBox();
+  }
+  adjust(labels) {
+    const self = this;
+    let label,
+      bbox,
+      x,
+      y;
+    const results = [];
+    for (let i = 0; i < labels.length; i++) {
+      label = labels[i];
+      x = label.attr('x');
+      y = label.attr('y');
+      for (let j = 0; j < 8; j++) {
+        bbox = self.adjustLabelPosition(label, x, y, j);
+        if (self.hasGap(bbox)) {
+          self.fillGap(bbox);
+          results.push(label);
+          break;
+        }
+      }
+    }
+    results[0].get('parent')._cfg.children = results;
+  }
+}
+
+module.exports = Greedy;
