3486fe9b0e87b17a54422041ed41b388ab60898e
Elaine1234 <yilin.qyl@antfin.com>
2018-09-28 22:39:44 +0800

    fix(label): fix greedy fill gap

--- src/label/utils/spiral-adjust.js
+++ src/label/utils/spiral-adjust.js
@@ -18,12 +18,14 @@ function spiralFill(label, greedy) {
     greedy.fillGap(bbox);
     return true;
   }
-  let canFill = false;
-  while (Math.min(Math.abs(dx), Math.abs(dy)) < maxDelta) {
+  let canFill = false,
+    times = 0;
+  while (Math.min(Math.abs(dx), Math.abs(dy)) < maxDelta && times < 20) {
     dxdy = f(t += dt);
     dx = ~~dxdy[0];
     dy = ~~dxdy[1];
     label.attr({ x: x + dx, y: y + dy });
+    times++;
     if (greedy.hasGap(label.getBBox())) {
       greedy.fillGap(bbox);
       canFill = true;
