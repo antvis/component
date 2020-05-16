939ec39ef2007e9c47201e1bece303fef5aa4f94
paleface001 <mipha.ly@antfin.com>
2020-05-16 23:58:32 +0800

    fix: 优化autoFormat逻辑，补充单测

--- src/axis/overlap/auto-format.ts
+++ src/axis/overlap/auto-format.ts
@@ -2,33 +2,51 @@ import { IElement, IGroup } from '@antv/g-base';
 import { each } from '@antv/util';
 import { strLen, testLabel } from './util';
 
-function formatLabel(label: IElement, unit: number, suffix: string, limitLength: number) {
+function formatLabel(label: IElement, unit: number, suffix: string, precision: number) {
     const text = label.attr('text');
     // 轴的零值标签不参与格式化
     if (text === '0') {
         return text;
     }
-    const labelLength = label.getBBox().width;
-    const codeLength = strLen(text);
-    const reseveLength = Math.floor((limitLength / labelLength) * codeLength) - 1;
     const value = parseFloat(text) / unit;
-    const newText = formatText(value,reseveLength);
+    const newText = formatText(value,precision);
     label.attr('text', `${newText}${suffix}`)
 }
 
-function formatText(value: number, reseveLength: number) {
+function getPrecision(labels:IElement[], unit: number, suffix: string, limitLength: number){
+    const values = [];
+    const length = [];
+    each(labels,(label)=>{
+        values.push(parseFloat(label.attr('text')) / unit);
+        length.push(label.getBBox().width);
+    });
+    values.sort((a,b)=>{
+        return b.toString().length - a.toString().length;
+    });
+    const maxLength = Math.max(...length);
+    const maxCodeLength = strLen(values[0].toString());
+    const suffixLength = strLen(suffix);
+    const reseveLength = Math.floor((limitLength / maxLength) * maxCodeLength) - suffixLength;
     // 先尝试保留小数点后两位
-    let valueCodeLength = strLen(value.toFixed(2).toString);
+    let valueCodeLength = strLen(values[0].toFixed(2).toString());
     if (valueCodeLength <= reseveLength) {
-        return value.toFixed(2);
+        return 2;
     }
     // 保留小数点后1位
-    valueCodeLength = strLen(value.toFixed(1).toString);
+    valueCodeLength = strLen(values[0].toFixed(1).toString());
     if (valueCodeLength <= reseveLength) {
-        return value.toFixed(1);
+        return 1
     }
     // 采用整数
-    return Math.round(value);
+    return 0;
+}
+
+function formatText(value: number, precision: number) {
+    if(precision === 0){
+       return Math.round(value); 
+    }else{
+        return value.toFixed(precision).toString();
+    }
 }
 
 export function formatLabels(labelGroup: IGroup, limitLength: number, unit: number, suffix: string): boolean {
@@ -39,8 +57,9 @@ export function formatLabels(labelGroup: IGroup, limitLength: number, unit: numb
         needFormat = needFormat || rst;
     });
     if (needFormat) {
+        const precision = getPrecision(children,unit,suffix,limitLength);
         each(children, (label) => {
-            formatLabel(label, unit, suffix, limitLength);
+            formatLabel(label, unit, suffix, precision);
         });
         return true;
     }
--- tests/unit/axis/auto-format-spec.ts
+++ tests/unit/axis/auto-format-spec.ts
@@ -1,118 +1,49 @@
 import { Canvas } from '@antv/g-canvas';
-import * as EllipsisUtil from '../../../src/axis/overlap/auto-ellipsis';
-import { getMatrixByAngle } from '../../../src/util/matrix';
+import * as AutoFormatUtil from '../../../src/axis/overlap/auto-format';
 
-describe('test axis label ellipsis', () => {
-  const dom = document.createElement('div');
-  document.body.appendChild(dom);
-  dom.id = 'cah';
-  const canvas = new Canvas({
-    container: 'cah',
-    width: 500,
-    height: 500,
-  });
-  const group = canvas.addGroup();
-  const labels = ['123', '12', '23444', '13455222', '2345', '2333', '222', '2222', '11', '33'];
-  const labels1 = ['我爱中国', '爱情ab你', 'abc我是谁', '亲爱的你ok', 'are u ok'];
-  function addLabels(arr, dx, dy, angle?) {
-    group.clear();
-    arr.forEach((label, index) => {
-      const x = 100 + dx * index;
-      const y = 100 + dy * index;
-      const shape = group.addShape({
-        type: 'text',
-        attrs: {
-          x,
-          y,
-          text: label,
-          fill: 'red',
-        },
-      });
-      if (angle) {
-        const matrix = getMatrixByAngle({ x, y }, angle);
-        shape.attr('matrix', matrix);
-      }
+describe('test axis label auto format', () => {
+    const dom = document.createElement('div');
+    document.body.appendChild(dom);
+    dom.id = 'car';
+    const canvas = new Canvas({
+      container: 'car',
+      width: 500,
+      height: 500,
     });
-  }
-
-  it('ellipsis tail, vetical, without rotation', () => {
-    addLabels(labels, 0, 20);
-    EllipsisUtil.ellipsisTail(true, group, 40);
-
-    const labelShapes = group.getChildren();
-    expect(labelShapes[3].attr('text')).not.toBe(labels[3]);
-    expect(labelShapes[3].get('tip')).toBe(labels[3]);
-  });
-
-  it('ellipsis tail, vetical, rotation', () => {
-    addLabels(labels1, 0, 20);
-    expect(EllipsisUtil.ellipsisTail(true, group, 40)).toBe(true);
-    // group.getChildren().forEach((label) => {
-    //   expect(label.getCanvasBBox().width <= 50).toBe(true);
-    // });
-    addLabels(labels1, 0, 20, -Math.PI / 4);
-    expect(EllipsisUtil.ellipsisTail(true, group, 35)).toBe(true);
-    // group.getChildren().forEach((label) => {
-    //   expect(label.getCanvasBBox().width <= 40).toBe(true);
-    // });
-  });
-
-  it('ellipsis head, horizontal', () => {
-    addLabels(labels, 20, 0);
-    EllipsisUtil.ellipsisTail(false, group, 30); // 不会进行任何省略
-    group.getChildren().forEach((label) => {
-      expect(label.get('tip')).toBe(null);
+    const group = canvas.addGroup();
+    const labels = ['333000', '534000', '50000', '9000', '345400'];
+    renderLabels(labels,group);
+    
+    it('format text',()=>{
+        const isFormatted = AutoFormatUtil.formatLabels(group,40,1000,'k');
+        expect(isFormatted).toBe(true);
+        const label = group.get('children')[0];
+        expect(label.getBBox().width).toBeLessThanOrEqual(40);
+        expect(label.attr('text')).toBe('333k');
     });
 
-    addLabels(labels, 20, 0, Math.PI / 4);
-    expect(EllipsisUtil.ellipsisTail(false, group, 30)).toBe(true);
-
-    // group.getChildren().forEach((label) => {
-    //   // 浮点数误差
-    //   expect(Math.floor(label.getCanvasBBox().height) <= 30).toBe(true);
-    // });
-
-    addLabels(labels1, 20, 0, Math.PI / 4);
-    expect(EllipsisUtil.ellipsisTail(false, group, 30)).toBe(true);
-  });
-
-  it('head', () => {
-    addLabels(labels, 0, 20);
-    EllipsisUtil.ellipsisHead(true, group, 40);
-    const labelShapes = group.getChildren();
-    expect(labelShapes[3].attr('text')).not.toBe(labels[3]);
-    expect(labelShapes[3].get('tip')).toBe(labels[3]);
-
-    addLabels(labels, 0, 20, -Math.PI / 4);
-    EllipsisUtil.ellipsisHead(true, group, 30);
-    const labelShapes1 = group.getChildren();
-    expect(labelShapes1[3].attr('text')).not.toBe(labels[3]);
-    expect(labelShapes1[3].get('tip')).toBe(labels[3]);
-
-    addLabels(labels, 20, 0, Math.PI / 4);
-    EllipsisUtil.ellipsisHead(false, group, 40);
-    const labelShapes2 = group.getChildren();
-    expect(labelShapes2[3].attr('text')).not.toBe(labels[3]);
-    expect(labelShapes2[3].get('tip')).toBe(labels[3]);
-  });
-
-  it('middle', () => {
-    addLabels(labels, 0, 20);
-    EllipsisUtil.ellipsisMiddle(true, group, 40);
-    const labelShapes = group.getChildren();
-    expect(labelShapes[3].attr('text')).not.toBe(labels[3]);
-    expect(labelShapes[3].get('tip')).toBe(labels[3]);
-
-    addLabels(labels, 0, 20);
-    EllipsisUtil.ellipsisMiddle(true, group, 60);
-    const labelShapes1 = group.getChildren();
-    expect(labelShapes1[3].attr('text')).toBe(labels[3]);
-    expect(labelShapes1[3].get('tip')).not.toBe(labels[3]);
-
-    addLabels(labels, 20, 0, Math.PI / 4);
-    EllipsisUtil.ellipsisMiddle(false, group, 40);
-    const labelShapes2 = group.getChildren();
-    expect(labelShapes2[3].attr('text')).not.toBe(labels[3]);
-    expect(labelShapes2[3].get('tip')).toBe(labels[3]);
-  });
+    it('not format text when space was enough',()=>{
+      renderLabels(labels,group);
+      const isFormatted = AutoFormatUtil.formatLabels(group,60,1000,'k');
+      expect(isFormatted).toBe(false);
+    });
 });
+
+function renderLabels(labels,group){
+  group.clear();
+  labels.forEach((label,index)=>{
+    const x = 100 + 80 * index;
+    const y = 100;
+    group.addShape({
+        type:'text',
+        attrs:{
+            x,
+            y,
+            text: label,
+            fill:'black',
+            textBaseline:'top'
+        }
+    });
+});
+
+}
