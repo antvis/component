6b213a41a452a9dc1c059d40e8e9fa51f2d50e7d
hustcc <i@hust.cc>
2020-10-12 16:32:36 +0800

    revert: axis auto-ellipsis

--- src/util/label.ts
+++ src/util/label.ts
@@ -19,10 +19,10 @@ export function getMaxLabelWidth(labels: IElement[]) {
   return max;
 }
 
-/** 获取label宽度高度 */
+/** 获取label长度 */
 export function getLabelLength(isVertical: boolean, label) {
-  const bbox = label.getCanvasBBox();
-  return isVertical ? [bbox.height, bbox.width] : [bbox.width, bbox.height];
+    const bbox = label.getCanvasBBox();
+    return isVertical ? bbox.width : bbox.height;
 }
 
 /* label长度是否超过约束值 */
@@ -31,40 +31,28 @@ export function testLabel(label: IElement, limitLength: number): boolean {
 }
 
 /** 处理 text shape 的自动省略 */
-/**
- * 
- * @param isVertical 是否水平
- * @param label 标签
- * @param verticalLimitLength 纵向限制大小
- * @param horizontalLimitLength 横向限制大小 undefined 则不限制
- * @param position 
- */
-export function ellipsisLabel(isVertical: boolean, label: IElement, verticalLimitLength: number, horizontalLimitLength: number, position: string = 'tail') {
+export function ellipsisLabel(isVertical: boolean, label: IElement, limitLength: number, position: string = 'tail') {
   const text = label.attr('text');
-  const [labelHorizontalLength, labelVerticalLength] = getLabelLength(isVertical, label);
+  const labelLength = getLabelLength(isVertical, label);
   const codeLength = strLen(text);
-
-  const ellipsisedByVertical = verticalLimitLength < labelVerticalLength;
-  // 如果为空，则设置最大的限制，即无限制
-  horizontalLimitLength = !horizontalLimitLength ? Number.MAX_SAFE_INTEGER : horizontalLimitLength;
-  const ellipsisedByHorizontal = horizontalLimitLength < labelHorizontalLength;
-
-  if (ellipsisedByVertical || ellipsisedByHorizontal) {
-    const reseveLengthByVertical = Math.floor((verticalLimitLength / labelVerticalLength) * codeLength) - ELLIPSIS_CODE_LENGTH; // 计算出来的应该保存的长度
-    const reseveLengthByHorizontal = Math.floor((horizontalLimitLength / labelHorizontalLength) * codeLength) - ELLIPSIS_CODE_LENGTH; // 计算出来的应该保存的长度
-
-    const reseveLength = Math.min(reseveLengthByVertical, reseveLengthByHorizontal);
-
-    const newText = reseveLength >= 0 ? ellipsisString(text, reseveLength, position) : ELLIPSIS_CODE;
-
+  let ellipsised = false;
+  if (limitLength < labelLength) {
+    const reseveLength = Math.floor((limitLength / labelLength) * codeLength) - ELLIPSIS_CODE_LENGTH; // 计算出来的应该保存的长度
+    let newText;
+    if (reseveLength >= 0) {
+      newText = ellipsisString(text, reseveLength, position);
+    } else {
+      newText = ELLIPSIS_CODE;
+    }
     if (newText) {
       label.attr('text', newText);
-      // tip 用于隐藏的时候 hover 显示全称  
-      label.set('tip', text);
-      return true;
+      ellipsised = true;
     }
   }
-
-  label.set('tip', null);
-  return false;
+  if (ellipsised) {
+    label.set('tip', text);
+  } else {
+    label.set('tip', null);
+  }
+  return ellipsised;
 }
