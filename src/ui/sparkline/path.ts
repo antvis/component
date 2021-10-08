5690cad9ad50a8c6cc2152410ce0a0fc734b954e
Aaron <943720372@qq.com>
2021-10-08 14:00:10 +0800

    revert: 变更了sparkline的曲线生成算法，避免出现线条超出上下边界的问题 #86 (#92)
    * Revert "refactor(sparkline): 变更了sparkline的曲线生成算法，避免出现线条超出上下边界的问题 (#86)"
    
    This reverts commit c51fa1c42e1f5bd0a63836afb569aa6429df7b55.
    
    * refactor: 更新了 @antv/path-util 版本至 2.0.15
    
    * docs(sparkline): update examples

--- src/ui/sparkline/path.ts
+++ src/ui/sparkline/path.ts
@@ -1,136 +1,7 @@
-import { vec2 } from '@antv/matrix-util';
 import { clone, isEqual } from '@antv/util';
+import { catmullRom2Bezier } from '@antv/path-util';
 import type { PathCommand } from '@antv/g';
 import type { Data, Line, Point, Scales } from './types';
-
-type Position = Point;
-
-function smoothBezier(points: Position[], smooth: number, isLoop: boolean, constraint: Position[]): Position[] {
-  const cps: Point[] = [];
-  const hasConstraint = !!constraint;
-
-  let prevPoint: Position;
-  let nextPoint: Position;
-  let min: Position;
-  let max: Position;
-  let nextCp0: Position;
-  let cp1: Position;
-  let cp0: Position;
-
-  if (hasConstraint) {
-    [min, max] = constraint;
-    for (let i = 0, l = points.length; i < l; i += 1) {
-      const point = points[i];
-      min = vec2.min([0, 0], min, point) as [number, number];
-      max = vec2.max([0, 0], max, point) as [number, number];
-    }
-  }
-
-  for (let i = 0, len = points.length; i < len; i += 1) {
-    const point = points[i];
-    if (i === 0 && !isLoop) {
-      cp0 = point;
-    } else if (i === len - 1 && !isLoop) {
-      cp1 = point;
-      cps.push(cp0!);
-      cps.push(cp1);
-    } else {
-      // isLoop ? (i ? i - 1 : len - 1) : i - 1;
-      const prevIdx = [i ? i - 1 : len - 1, i - 1][isLoop ? 0 : 1];
-      prevPoint = points[prevIdx];
-      nextPoint = points[isLoop ? (i + 1) % len : i + 1];
-
-      let v: [number, number] = [0, 0];
-      v = vec2.sub(v, nextPoint, prevPoint) as [number, number];
-      v = vec2.scale(v, v, smooth) as [number, number];
-
-      let d0 = vec2.distance(point, prevPoint);
-      let d1 = vec2.distance(point, nextPoint);
-
-      const sum = d0 + d1;
-      if (sum !== 0) {
-        d0 /= sum;
-        d1 /= sum;
-      }
-
-      let v1 = vec2.scale([0, 0], v, -d0);
-      let v2 = vec2.scale([0, 0], v, d1);
-
-      cp1 = vec2.add([0, 0], point, v1) as Position;
-      nextCp0 = vec2.add([0, 0], point, v2) as Position;
-
-      // 下一个控制点必须在这个点和下一个点之间
-      nextCp0 = vec2.min([0, 0], nextCp0, vec2.max([0, 0], nextPoint, point)) as Position;
-      nextCp0 = vec2.max([0, 0], nextCp0, vec2.min([0, 0], nextPoint, point)) as Position;
-
-      // 重新计算 cp1 的值
-      v1 = vec2.sub([0, 0], nextCp0, point);
-      v1 = vec2.scale([0, 0], v1, -d0 / d1);
-      cp1 = vec2.add([0, 0], point, v1) as Position;
-
-      // 上一个控制点必须要在上一个点和这一个点之间
-      cp1 = vec2.min([0, 0], cp1, vec2.max([0, 0], prevPoint, point)) as Position;
-      cp1 = vec2.max([0, 0], cp1, vec2.min([0, 0], prevPoint, point)) as Position;
-
-      // 重新计算 nextCp0 的值
-      v2 = vec2.sub([0, 0], point, cp1);
-      v2 = vec2.scale([0, 0], v2, d1 / d0);
-      nextCp0 = vec2.add([0, 0], point, v2) as Position;
-
-      if (hasConstraint) {
-        cp1 = vec2.max([0, 0], cp1, min!) as Position;
-        cp1 = vec2.min([0, 0], cp1, max!) as Position;
-        nextCp0 = vec2.max([0, 0], nextCp0, min!) as Position;
-        nextCp0 = vec2.min([0, 0], nextCp0, max!) as Position;
-      }
-
-      cps.push(cp0!);
-      cps.push(cp1);
-      cp0 = nextCp0;
-    }
-  }
-
-  if (isLoop) {
-    cps.push(cps.shift()!);
-  }
-
-  return cps;
-}
-
-function catmullRom2bezier(crp: number[], z: boolean, constraint: Position[]): PathCommand[] {
-  console.log('new catmullRom2bezier');
-
-  const isLoop = !!z;
-  const pointList: Point[] = [];
-  for (let i = 0, l = crp.length; i < l; i += 2) {
-    pointList.push([crp[i], crp[i + 1]]);
-  }
-
-  const controlPointList = smoothBezier(pointList, 0.4, isLoop, constraint);
-  const len = pointList.length;
-  const d1: PathCommand[] = [];
-
-  let cp1: Position;
-  let cp2: Position;
-  let p: Position;
-
-  for (let i = 0; i < len - 1; i += 1) {
-    cp1 = controlPointList[i * 2];
-    cp2 = controlPointList[i * 2 + 1];
-    p = pointList[i + 1];
-
-    d1.push(['C', cp1[0], cp1[1], cp2[0], cp2[1], p[0], p[1]]);
-  }
-
-  if (isLoop) {
-    cp1 = controlPointList[len];
-    cp2 = controlPointList[len + 1];
-    [p] = pointList;
-
-    d1.push(['C', cp1[0], cp1[1], cp2[0], cp2[1], p[0], p[1]]);
-  }
-  return d1;
-}
 
 /**
  * 根据数据获得每条线各点x，y值
@@ -171,10 +42,7 @@ export function lineToCurvePath(line: Line, reverse = false) {
       data.push(...point);
     }
   }
-  const path = catmullRom2bezier(data, false, [
-    [0, 0],
-    [1, 1],
-  ]);
+  const path = catmullRom2Bezier(data, false);
   if (reverse) {
     path.unshift(['M', ...line[len - 1]]);
   } else {
