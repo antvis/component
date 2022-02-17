96cfe5e169df49435fbd432c4e99fa518f77e5bd
visiky <736929286@qq.com>
2022-02-17 16:57:35 +0800

    chore: 移除 webpack5.x, ts-loader 降级为 7.x (#101)

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,26 @@ module.exports = {
     filename: 'gui.min.js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              // 可以提高构建速度。缺点：没有进行类型编译检查。
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   externals: {
     '@antv/g': {
