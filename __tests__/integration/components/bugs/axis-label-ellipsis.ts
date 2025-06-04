1f756fe640866a70e00f16d5e53f9ae798c1ffd5
szxc <shuzhuxvchuang@163.com>
2025-06-04 17:14:45 +0800

    test:  添加测试

---
+++ __tests__/integration/components/bugs/axis-label-ellipsis.ts
@@ -0,0 +1,38 @@
+import { Group } from '@antv/g';
+import { Axis } from '../../../../src/ui/axis';
+import { mockData } from '../../utils/mock-data';
+
+export const BugAxisLabelEllipsis = () => {
+  const group = new Group({
+    style: { width: 1000, height: 300 },
+  });
+
+  group.appendChild(
+    new Axis({
+      style: {
+        data: mockData,
+        labelOverlap: [
+          {
+            type: 'ellipsis',
+            suffix: '...',
+            maxLength: 50,
+            minLength: 20,
+            step: 5,
+          },
+        ],
+        startPos: [50, 50],
+        endPos: [900, 50],
+        lineLineWidth: 2,
+        lineStroke: 'black',
+        tickLineWidth: 2,
+        tickStroke: 'black',
+        labelSpacing: 10,
+        type: 'linear',
+      },
+    })
+  );
+
+  return group;
+};
+
+BugAxisLabelEllipsis.tags = ['轴标签', '省略号处理', '单标签支持', 'bug修复'];
