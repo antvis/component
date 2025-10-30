a0ba2a4f95c752e52e0298f9e0206b215c2ba217
interstellarmt <787982239@qq.com>
2025-10-30 19:45:40 +0800

    feat: standardize the className of the axis

--- src/ui/axis/axis.ts
+++ src/ui/axis/axis.ts
@@ -11,6 +11,8 @@ import { renderAxisLine } from './guides/line';
 import { renderTicks } from './guides/ticks';
 import { renderTitle } from './guides/title';
 import type { AxisDatum, AxisOptions, AxisStyleProps, RequiredAxisStyleProps } from './types';
+import { applyClassName, getAxisClassName } from './utils/classname';
+import { CLASSNAME_SUFFIX_MAP } from './classname-map';
 
 export type {
   ArcAxisOptions,
@@ -27,9 +29,10 @@ function renderAxisMain(
   data: AxisDatum[],
   animation: StandardAnimationOption
 ) {
-  const { showLine, showTick, showLabel } = attributes;
+  const { showLine, showTick, showLabel, classNamePrefix } = attributes;
   /** line */
   const lineGroup = container.maybeAppendByClassName(CLASS_NAMES.lineGroup, 'g');
+  applyClassName(lineGroup, CLASS_NAMES.lineGroup, CLASSNAME_SUFFIX_MAP.lineGroup, classNamePrefix);
   const lineTransitions =
     ifShow(showLine!, lineGroup, (group) => {
       return renderAxisLine(group, attributes, animation);
@@ -37,6 +40,7 @@ function renderAxisMain(
 
   /** tick */
   const tickGroup = container.maybeAppendByClassName(CLASS_NAMES.tickGroup, 'g');
+  applyClassName(tickGroup, CLASS_NAMES.tickGroup, CLASSNAME_SUFFIX_MAP.tickGroup, classNamePrefix);
   const tickTransitions =
     ifShow(showTick!, tickGroup, (group) => {
       return renderTicks(group, data, attributes, animation);
@@ -44,6 +48,7 @@ function renderAxisMain(
 
   /** label */
   const labelGroup = container.maybeAppendByClassName(CLASS_NAMES.labelGroup, 'g');
+  applyClassName(labelGroup, CLASS_NAMES.labelGroup, CLASSNAME_SUFFIX_MAP.labelGroup, classNamePrefix);
   const labelTransitions =
     ifShow(showLabel!, labelGroup, (group) => {
       return renderLabels(group, data, attributes, animation, container.node());
@@ -58,7 +63,7 @@ export class Axis extends Component<AxisStyleProps> {
   }
 
   render(attributes: RequiredAxisStyleProps, container: Group, specificAnimation?: GenericAnimation) {
-    const { titleText, data, animate, showTitle, showGrid, dataThreshold, truncRange } = attributes;
+    const { titleText, data, animate, showTitle, showGrid, dataThreshold, truncRange, classNamePrefix } = attributes;
     const sampledData = sampling(data, dataThreshold).filter(({ value }) => {
       if (truncRange && value > truncRange[0] && value < truncRange[1]) return false;
       return true;
@@ -68,11 +73,13 @@ export class Axis extends Component<AxisStyleProps> {
 
     /** grid */
     const gridGroup = select(container).maybeAppendByClassName(CLASS_NAMES.gridGroup, 'g');
+    applyClassName(gridGroup, CLASS_NAMES.gridGroup, CLASSNAME_SUFFIX_MAP.gridGroup, classNamePrefix);
     const gridTransitions =
       ifShow(showGrid!, gridGroup, (group) => renderGrid(group, sampledData, attributes, finalAnimation)) || [];
 
     /** main group */
     const mainGroup = select(container).maybeAppendByClassName(CLASS_NAMES.mainGroup, 'g');
+    applyClassName(mainGroup, CLASS_NAMES.mainGroup, CLASSNAME_SUFFIX_MAP.mainGroup, classNamePrefix);
 
     if (titleText && ((!this.initialized && finalAnimation.enter) || (this.initialized && finalAnimation.update))) {
       renderAxisMain(attributes, select(this.offscreenGroup), sampledData, parseAnimationOption(false));
@@ -81,6 +88,7 @@ export class Axis extends Component<AxisStyleProps> {
     const mainTransitions = renderAxisMain(attributes, select(mainGroup.node()), sampledData, finalAnimation);
     /** title */
     const titleGroup = select(container).maybeAppendByClassName(CLASS_NAMES.titleGroup, 'g');
+    applyClassName(titleGroup, CLASS_NAMES.titleGroup, CLASSNAME_SUFFIX_MAP.titleGroup, classNamePrefix);
     const titleTransitions =
       ifShow(showTitle, titleGroup, (group) => {
         return renderTitle(group, this, attributes, finalAnimation);
