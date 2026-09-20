e7fa87daa1dc5f0693048dc819a60ecacf13256e
hustcc <i@hust.cc>
2026-09-20 17:19:45 +0800

    Use optional chaining for animation cancellation

--- src/ui/navigator/index.ts
+++ src/ui/navigator/index.ts
@@ -146,7 +146,7 @@ export class Navigator extends Component<NavigatorStyleProps> {
     );
 
     onAnimateFinished(animation, () => {
-      animation.cancel();
+      animation?.cancel();
       this.innerCurrPage = pageNum;
       this.playState = 'idle';
       this.setVisiblePages([pageNum]);
